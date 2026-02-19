a23952ccb98de96b230aefcbda06457b8a93c05d
pacuito121 <corexmartex@gmail.com>
2026-02-19 00:50:50 -0700

    Add files via upload

---
+++ JS/func.js
@@ -0,0 +1,199 @@
+
+let doc1_Main = {
+    "1":{
+        "Nombre":"Erick Adigard",
+        "MusicUrl":"Img/Hero!.mp3",
+        "ImgUrl":"Img/ErikAdigard.jpg",
+        "Data":"M.A.D. (McShane Adigard Design)\n Diseñador gráfico fundador de M.A.D. en 1989.Su trabajo abarca sitios web, instalaciones multimedia y publicaciones impresas para clientes internacionales como la revista Wired. Representa el diseño vinculado a los medios digitales y la comunicación visual contemporánea."
+    },
+    "2":{
+        "Nombre":"Rebeca Mendez",
+        "MusicUrl":"Img/Elefante - Ángel (LetraLyrics).mp3",
+        "ImgUrl":"Img/RebecaMendez.jpg",
+        "Data":"Méndez Communications \n Diseñadora gráfica mexicana formada en Art Center College of Design. Ha trabajado para instituciones como el Getty Center y el Whitney Museum of American Art. Representa el diseño como lenguaje cultural e institucional."
+    },
+    "3":{
+        "Nombre":"Julie Bargmann",
+        "MusicUrl":"Img/Disturbed - Hold on to Memories [Official Music Video].mp3",
+        "ImgUrl":"Img/BargmannJulie.jpg",
+        "Data":"D.I.R.T. Studio \n Fundadora de D.I.R.T. Studio en 1992. Ha realizado proyectos de paisaje para el Massachusetts Museum of Contemporary Art y parques urbanos en Nueva York.Representa el diseño como herramienta de transformación ambiental y social."
+    },
+    "4":{
+        "Nombre":"Michael Gabellini",
+        "MusicUrl":"Img/Si tú no estás aquí.mp3",
+        "ImgUrl":"Img/MichaelGab.jpg",
+        "Data":"Gabellini Associates \nArquitecto y diseñador formado en Rhode Island School of Design. Ha desarrollado exposiciones para el Guggenheim Museum y la Council of Fashion Designers of America. Representa el diseño ligado a la arquitectura, los espacios culturales y la moda."
+    }
+}
+let doc2_Data = {
+    "Txt":{
+        1:{
+            1:"<strong>Diseño para Wired Magazine</strong><br>Fue colaborador y director de arte, creando ensayos visuales e innovadores conceptos digitales que marcaron la estética de la revista en los años 90.",
+            2:"<strong>Identidad visual de IBM Software</strong><br>Desarrolló un sistema gráfico que integraba tecnología, negocio y ecología desde una perspectiva humanista.",
+            3:"<strong>Spontaneous Interventions (Bienal de Arquitectura de Venecia)</strong><br>Participó en proyectos que exploraban la relación entre diseño, espacio urbano y participación social."
+        },
+        2:{
+            1:"<strong>Premios de excelencia en diseño</strong><br>Ganó el Cooper-Hewitt National Design Award en Comunicación (2012), la Medalla AIGA (2017) y fue incluida en el One Club Hall of Fame (2017), siendo la primera latina en lograr los tres.",
+            2:"<strong>Counterforce Lab en UCLA</strong><br>Fundó este laboratorio de investigación y diseño enfocado en la relación entre cultura, ecología y medios.",
+            3:"<strong>Instalaciones de video y arte público</strong><br>Ha creado obras permanentes y colaboraciones con arquitectos, explorando la naturaleza y la cultura en lugares como el Sahara, Patagonia e Islandia."
+        },
+        3:{
+            1:"<strong>Fundadora de D.I.R.T. Studio</strong><br>Estudio pionero en transformar paisajes degradados (minas, terrenos industriales) en espacios regenerativos.",
+            2:"<strong>Cornelia Hahn Oberlander Prize</strong><br>Fue la primera ganadora de este prestigioso premio internacional en arquitectura del paisaje.",
+            3:"<strong>Investigación sobre sitios tóxicos y urbanos</strong><br>Desarrolló metodologías de “critical site-seeing” para revelar historias ocultas y dar nuevas posibilidades a comunidades en entornos dañados."
+        },
+        4:{
+            1:"<strong>Innovación en “Spatial Branding”</strong><br>Introdujo un enfoque que integra identidad de marca con arquitectura, creando experiencias espaciales únicas para clientes de moda y cultura.",
+            2:"<strong>Premio Nacional de Diseño en Interiores (2006)</strong><br>Reconocido por la excelencia en arquitectura interior y diseño minimalista.",
+            3:"<strong>Fundador de Gabellini Sheppard Associates</strong><br>Ha diseñado boutiques, hoteles y espacios culturales con un enfoque en la simplicidad espacial y el uso de la luz."
+        }
+    },
+    "Imgs":{
+        1:{
+            1:"Img/Azan/WiredMag.webp",
+            2:"Img/Azan/IBM-Software.jpg",
+            3:"Img/Azan/VBienal.jpg"
+        },
+        2:{
+            1:"Img/Azan/Awards.jpg",
+            2:"Img/Azan/Ucla.png",
+            3:"Img/Azan/Vidio.png"
+        },
+        3:{
+            1:"Img/Azan/Dirt.jpg",
+            2:"Img/Azan/Cornelia.jpg",
+            3:"Img/Azan/toxicos.jpg"
+        },
+        4:{
+            1:"Img/Azan/gs.jpg",
+            2:"Img/Azan/Expo.jpg",
+            3:"Img/Azan/Branding.jpg"
+        }
+    }
+}
+let doc3_Vids = {
+    "1":{
+        "Name":"This is Design Culture Now...and now Design Chronology",
+        "Url":"https://www.youtube.com/watch?v=9zyzuLVKYrA&pp=ygUSZGVzaWduIGN1bHR1cmUgbm93",
+        "Img":"Img/Vid1.png"
+    },
+    "2":{
+        "Name":"Design Culture is Everything",
+        "Url":"https://www.youtube.com/watch?v=JVsYjdJ6sz8",
+        "Img":"Img/Vid2.png"
+    },
+    "3":{
+        "Name":'The Secret To "Great" Design Is Simpler Than You Think',
+        "Url":"https://www.youtube.com/watch?v=SdCzIDmfDAA",
+        "Img":"Img/Vid3.png"
+    },
+    "4":{
+        "Name":"How A Graphic Designer MUST Think in 2026 To Survive!",
+        "Url":"https://www.youtube.com/watch?v=WFkGbyH24AY",
+        "Img":"Img/Vid4.webp"
+    },
+    "5":{
+        "Name":"Introducing Julie Bargmann, the 2021 Oberlander Prize Laureate",
+        "Url":"https://www.youtube.com/watch?v=xy31vNsIG1M",
+        "Img":"Img/Vid5.png"
+    },
+    "6":{
+        "Name":"Michael Gabellini - SPACE: LIGHT",
+        "Url":"https://www.youtube.com/watch?v=ls2zYHrARvc",
+        "Img":"Img/Vid6.png"
+    },
+    "7":{
+        "Name":"How does culture influence modern design? | Culture By Design | Ep 1| ABC Australia",
+        "Url":"https://www.youtube.com/watch?v=U1L0co41-IY",
+        "Img":"Img/Vid7.png"
+    },
+}
+let dow = document.querySelectorAll(".opt")
+let play = new Audio();
+let bPlay = document.getElementById("play")
+let buts = document.getElementById("vin")
+bPlay.addEventListener("click",()=>{
+    if(play.paused){
+        play.play()
+        buts.classList.add("moveBote")
+    }else{
+        play.pause()
+        buts.classList.remove("moveBote")
+    }
+    
+},false)
+
+let dis = false
+let set = 0
+function dd(){
+    if(!dis){
+        dis= true
+        document.getElementById("img").style = "display:flex;"
+    }else{
+        dis=false
+        document.getElementById("img").style = "display:none;"
+    }
+}
+function squirrel(a,n){
+    document.getElementById("carro").innerHTML = "";
+    (a) ? set++ : set--;
+    console.log(set)
+    if(set<=0){set = 7}
+    else if(set>=8) {set = 1}
+    console.log(set)
+    let av= (n==0)? set : n;
+    console.log (av)
+    for(let i=0;i<5;i++){
+        let b = document.createElement("img")
+        b.src = doc3_Vids[`${av}`]["Img"]
+        console.log(b.src.toString())
+        console.log("Av = "+av)
+        if(i==2){
+            b.className =  "Selec"
+            document.getElementById("tit").innerHTML = doc3_Vids[`${av}`]["Name"]
+            b.addEventListener("click",()=>{
+                location.href = doc3_Vids[`${av}`]["Url"]
+                console.log("Estoy vivo!")
+            })
+        }else{
+            b.className = "Another"
+            let nix = av
+            for(let j=2;j>0;j--){
+                nix--
+                if(nix < 1){
+                    nix = 7
+                }
+            }
+            b.id = nix
+            b.addEventListener("click",()=>{
+                console.log("The iD caller is: "+b.id)
+                squirrel(1,parseInt(b.id))
+            })
+        }
+        document.getElementById("carro").appendChild(b)
+        av++
+        if(av>=8 || av<=0){
+            av = (av>=8) ? 1 : 7;
+        }
+    }
+    
+}
+function pet(a){
+    document.getElementById("A").setAttribute("src",doc1_Main[a].ImgUrl)
+    document.getElementById("txt").value = doc1_Main[a].Data
+    document.getElementById("name").children.item(0).innerHTML = doc1_Main[a].Nombre
+    play.pause()
+    buts.classList.remove("moveBote")
+    play.src = doc1_Main[a].MusicUrl
+}
+
+function opxis(a){
+    let teer = document.querySelectorAll(".info")
+    for(let i=0;i<3;i++){
+        teer.item(i).children.item(0).setAttribute("src",doc2_Data.Imgs[a][i+1])
+        teer.item(i).children.item(1).innerHTML = doc2_Data.Txt[a][i+1]
+        console.log(doc2_Data.Imgs[a][i+1])
+        console.log(doc2_Data.Txt[a][i+1])
+    }
+}
+document.addEventListener("DOMContentLoaded",squirrel(1,0))
